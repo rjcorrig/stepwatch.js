ec792fc32dc51041614f58730a7c88f1710b435f
Robert Corrigan <rjcorrig@gmail.com>
2017-11-28 06:11:37 -0500

    Wire in Step component to Run component

--- src/components/Run.vue
+++ src/components/Run.vue
@@ -3,20 +3,37 @@
     <h1 class="sw-header">
         {{ run.name }}
     </h1>
-    <ol class="sw-runstep-list">
+    <ol class="sw-runstep-list" v-if="run.steps.length">
+      <Step v-for="step in run.steps" :step="step" :key="step.id" />
     </ol>
+    <p v-else>
+      No steps defined
+    </p>
   </div>
 </template>
 
 <script>
 import models from '@/stepwatch/models'
+import Step from './Step.vue'
 
 export default {
   name: 'Run',
   data () {
+    var run = new models.Run()
+    var step1 = new models.Step({ name: 'Walk for 5 seconds', totalSeconds: 5 })
+    var step2 = new models.Step({ name: 'Run for 15 seconds', totalSeconds: 15 })
+    var step3 = new models.Step({ name: 'Walk for 5 seconds', totalSeconds: 5 })
+
+    run.steps = [ step1, step2, step3 ]
+    run.steps.forEach(function (step, i) {
+      step.id = i
+    })
     return {
-      run: new models.Run()
+      run: run
     }
+  },
+  components: {
+    Step
   }
 }
 </script>
---
+++ src/components/Step.vue
@@ -0,0 +1,47 @@
+<template>
+  <li class="sw-runstep sw-card">
+    <div class="sw-runstep-text">
+        {{ step.name }}
+    </div>
+    <div class="sw-runstep-timer">
+        {{ step.runSeconds }}
+    </div>
+    <button class="sw-runstep-toggle sw-button">Start</button>
+  </li>
+</template>
+
+<script>
+import models from '@/stepwatch/models'
+
+export default {
+  name: 'Step',
+  props: {
+    step: {
+      type: models.Step,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+.sw-runstep-timer {
+  margin: 30px auto;
+  font-size: xx-large;
+}
+
+.sw-button {
+  margin: auto;
+  font-size: xx-large;
+  height: 1.5em;
+  width: 6em;
+}
+
+.sw-card {
+  padding: 5px 5px 10px 5px;
+  text-align: center;
+  width: 300px;
+  margin: 10px auto;
+  box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+}
+</style>
