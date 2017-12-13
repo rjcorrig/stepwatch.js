ddcb205ec7fca3955469f84f8572e837f9f0cfee
Robert Corrigan <rjcorrig@gmail.com>
2017-12-12 20:14:10 -0500

    Add home view and route

---
+++ src/components/sw-home.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="sw-page sw-home">
+    <h1 class="sw-header">Welcome to StepWatch</h1>
+    <ol class="sw-card-list">
+      <li>
+        <div class="sw-card">
+          <div class="sw-category-text">
+            Programs defined
+          </div>
+          <div class="sw-category-count">
+            {{ programCount }}
+          </div>
+          <button class="sw-button">{{ whereNext }}</button>
+        </div>
+      </li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'sw-home',
+  data () {
+    return {
+      runs: this.$services.dataStore.getRuns(
+        r => r.status === 'program'
+      )
+    }
+  },
+  computed: {
+    programCount () {
+      return this.runs.length
+    },
+    whereNext () {
+      if (this.programCount === 0) {
+        return 'Create New'
+      } else {
+        return 'Go to List'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.sw-category-count {
+  margin: 30px auto;
+  font-size: xx-large;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import swRun from '@/components/sw-run'
+import swHome from '@/components/sw-home'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/',
+      name: 'sw-home',
+      component: swHome
+    },
     {
       path: '/run/:id',
       name: 'sw-run',
