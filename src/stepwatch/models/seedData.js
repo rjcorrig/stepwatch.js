b1dba20c3807ab5a1e6eeca75aeeadfd3f1587d4
Robert Corrigan <rjcorrig@gmail.com>
2017-12-15 06:17:24 -0500

    Add more seedData

--- src/stepwatch/models/seedData.js
+++ src/stepwatch/models/seedData.js
@@ -20,11 +20,12 @@ import Run from './run'
 import Step from './step'
 
 export default [
+  // The sprint program
   new Run({
     id: 'foo',
     name: 'Sprint',
     status: 'program',
-    totalSeconds: 0,
+    totalSeconds: 5 + 15 + 5,
     runSeconds: 0,
     currentStep: null,
     startTime: null,
@@ -53,6 +54,168 @@ export default [
         runSeconds: 0,
         startTime: null,
         endTime: null
+      })
+    ]
+  }),
+  // The pie program
+  new Run({
+    id: 'baz',
+    name: 'Make a pie',
+    status: 'program',
+    totalSeconds: 60 * 60,
+    runSeconds: 0,
+    currentStep: null,
+    startTime: null,
+    endTime: null,
+    steps: [
+      new Step({
+        name: 'Bake at 350 for 40 minutes',
+        status: 'created',
+        totalSeconds: 60 * 40,
+        runSeconds: 0,
+        startTime: null,
+        endTime: null
+      }),
+      new Step({
+        name: 'Cool for 20 minutes',
+        status: 'created',
+        totalSeconds: 20 * 60,
+        runSeconds: 0,
+        startTime: null,
+        endTime: null
+      })
+    ]
+  }),
+  // A created but not started run of the pie program
+  new Run({
+    id: 'qux',
+    name: 'Make a pie',
+    status: 'created',
+    totalSeconds: 60 * 60,
+    runSeconds: 0,
+    currentStep: null,
+    startTime: null,
+    endTime: null,
+    steps: [
+      new Step({
+        name: 'Bake at 350 for 40 minutes',
+        status: 'created',
+        totalSeconds: 60 * 40,
+        runSeconds: 0,
+        startTime: null,
+        endTime: null
+      }),
+      new Step({
+        name: 'Cool for 20 minutes',
+        status: 'created',
+        totalSeconds: 20 * 60,
+        runSeconds: 0,
+        startTime: null,
+        endTime: null
+      })
+    ]
+  }),
+  // A paused run of the Sprint program, paused just now
+  new Run({
+    id: 'quux',
+    name: 'Sprint',
+    status: 'paused',
+    totalSeconds: 5 + 15 + 5,
+    runSeconds: 5 + 7,
+    currentStep: 1,
+    startTime: Date.now() - 12000,
+    endTime: null,
+    steps: [
+      new Step({
+        name: 'Walk for 5 seconds',
+        status: 'complete',
+        totalSeconds: 5,
+        runSeconds: 5,
+        startTime: Date.now() - 12000,
+        endTime: Date.now() - 7000
+      }),
+      new Step({
+        name: 'Run for 15 seconds',
+        status: 'paused',
+        totalSeconds: 15,
+        runSeconds: 7,
+        startTime: Date.now() - 7000,
+        endTime: null
+      }),
+      new Step({
+        name: 'Walk for 5 seconds',
+        status: 'created',
+        totalSeconds: 5,
+        runSeconds: 0,
+        startTime: null,
+        endTime: null
+      })
+    ]
+  }),
+  // A canceled run of the pie program
+  new Run({
+    id: 'corge',
+    name: 'Make a pie',
+    status: 'canceled',
+    totalSeconds: 60 * 60,
+    runSeconds: 20 * 60,
+    currentStep: 0,
+    startTime: Date.now() - 3600 * 1000,
+    endTime: Date.now() - 2400 * 1000,
+    steps: [
+      new Step({
+        name: 'Bake at 350 for 40 minutes',
+        status: 'canceled',
+        totalSeconds: 60 * 40,
+        runSeconds: 60 * 20,
+        startTime: Date.now() - 3600 * 1000,
+        endTime: Date.now() - 2400 * 1000
+      }),
+      new Step({
+        name: 'Cool for 20 minutes',
+        status: 'canceled',
+        totalSeconds: 20 * 60,
+        runSeconds: 0,
+        startTime: null,
+        endTime: null
+      })
+    ]
+  }),
+  // A completed sprint
+  // A paused run of the Sprint program, paused just now
+  new Run({
+    id: 'grault',
+    name: 'Sprint',
+    status: 'complete',
+    totalSeconds: 5 + 15 + 5,
+    runSeconds: 5 + 15 + 5,
+    currentStep: 2,
+    startTime: Date.now() - 30 * 60 * 1000,
+    endTime: Date.now() - (30 * 60 - 25) * 1000,
+    steps: [
+      new Step({
+        name: 'Walk for 5 seconds',
+        status: 'complete',
+        totalSeconds: 5,
+        runSeconds: 5,
+        startTime: Date.now() - 30 * 60 * 1000,
+        endTime: Date.now() - (30 * 60 - 5) * 1000
+      }),
+      new Step({
+        name: 'Run for 15 seconds',
+        status: 'complete',
+        totalSeconds: 15,
+        runSeconds: 15,
+        startTime: Date.now() - (30 * 60 - 5) * 1000,
+        endTime: Date.now() - (30 * 60 - 20) * 1000
+      }),
+      new Step({
+        name: 'Walk for 5 seconds',
+        status: 'complete',
+        totalSeconds: 5,
+        runSeconds: 5,
+        startTime: Date.now() - (30 * 60 - 20) * 1000,
+        endTime: Date.now() - (30 * 60 - 25) * 1000
       })
     ]
   })
