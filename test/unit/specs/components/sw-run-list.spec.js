5a95e392add4321d31faebf20e1c965a81a79029
Robert Corrigan <rjcorrig@gmail.com>
2017-12-16 15:42:52 -0500

    Add sw-run-list tests

---
+++ test/unit/specs/components/sw-run-list.spec.js
@@ -0,0 +1,60 @@
+import Vue from 'vue'
+import swRunList from '@/components/sw-run-list'
+import seedData from '@/stepwatch/models/seedData'
+import servicePlugin from '@/plugins/services'
+import DataStore from '@/stepwatch/services/datastore'
+
+// Rig up and use the mock dataStore
+var dataStore = new DataStore()
+Vue.use(servicePlugin, {
+  dataStore: dataStore
+})
+
+describe('sw-run-list.vue', () => {
+  before(() => {
+    // Set up the test data
+    dataStore.seed(seedData)
+  })
+
+  it('should list all runs if no props passed', () => {
+    const Constructor = Vue.extend(swRunList)
+    const vm = new Constructor().$mount()
+    expect(vm.$el.querySelector('.sw-run-list > h1').textContent)
+      .to.equal('All runs and programs')
+    expect(vm.$el.querySelectorAll('li.sw-run-item').length)
+    .to.equal(dataStore.getRuns().length)
+  })
+
+  it('should list only filtered runs if filter was passed', () => {
+    const Constructor = Vue.extend(swRunList)
+    const propsData = {
+      title: 'pie',
+      filter: r => r.name.indexOf('pie') >= 0
+    }
+    const vm = new Constructor({ propsData }).$mount()
+    expect(vm.$el.querySelector('.sw-run-list > h1').textContent)
+      .to.equal(propsData.title)
+    expect(vm.$el.querySelectorAll('li.sw-run-item').length)
+      .to.equal(dataStore.getRuns(propsData.filter).length)
+  })
+
+  it('shows the Create New button for program list', () => {
+    const Constructor = Vue.extend(swRunList)
+    const propsData = {
+      type: 'program'
+    }
+    const vm = new Constructor({ propsData }).$mount()
+    expect(vm.$el.querySelector('.sw-run-list > button'))
+      .to.not.equal(null)
+  })
+
+  it('hides the Create New button for non-program list', () => {
+    const Constructor = Vue.extend(swRunList)
+    const propsData = {
+      type: 'pies'
+    }
+    const vm = new Constructor({ propsData }).$mount()
+    expect(vm.$el.querySelector('.sw-run-list > button'))
+      .to.equal(null)
+  })
+})
