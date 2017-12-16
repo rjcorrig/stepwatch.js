44081a1c74eecd0e5556a982bc84f258178ef3d0
Robert Corrigan <rjcorrig@gmail.com>
2017-12-15 21:43:55 -0500

    Use filter function in sw-run-list

--- src/components/sw-run-list.vue
+++ src/components/sw-run-list.vue
@@ -27,31 +27,8 @@ export default {
     }
   },
   data () {
-    var runs
-
-    switch (this.type) {
-      case 'program': {
-        runs = this.$services.dataStore.getRuns(
-          r => r.status === 'program'
-        )
-        break
-      }
-      case 'running': {
-        runs = this.$services.dataStore.getRuns(
-          r => ['paused', 'running', 'created'].indexOf(r.status) >= 0
-        )
-        break
-      }
-      case 'history': {
-        runs = this.$services.dataStore.getRuns(
-          r => ['canceled', 'complete'].indexOf(r.status) >= 0
-        )
-        break
-      }
-    }
-
     return {
-      runs: runs
+      runs: this.$services.dataStore.getRuns(this.filter)
     }
   },
   components: {
