7b91a1f02bb4f7b50277859fa3d7f0deee915d40
Robert Corrigan <rjcorrig@gmail.com>
2017-12-16 22:32:15 -0500

    Add navigation e2e test

---
+++ test/e2e/specs/navigation.js
@@ -0,0 +1,22 @@
+// For authoring Nightwatch tests, see
+// http://nightwatchjs.org/guide#usage
+
+module.exports = {
+  'home page to run list and back': function (browser) {
+    const devServer = browser.globals.devServerURL
+
+    browser
+      .url(devServer + '/#')
+      .waitForElementVisible('#app', 5000)
+      .assert.elementPresent('.sw-home')
+      .assert.containsText('h1', 'Welcome')
+      .assert.elementPresent('.sw-home .sw-card-list li:first-child .sw-button')
+      .click('.sw-home .sw-card-list li:first-child .sw-button')
+      .waitForElementVisible('.sw-run-list', 5000)
+      .assert.containsText('h1', 'Programs')
+      .back()
+      .waitForElementVisible('.sw-home', 5000)
+      .assert.containsText('h1', 'Welcome')
+      .end()
+  }
+}
