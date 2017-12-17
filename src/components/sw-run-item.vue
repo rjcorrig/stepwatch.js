5b0da5b58781e5263b3c91e5e823021ad99088f7
Robert Corrigan <rjcorrig@gmail.com>
2017-12-16 23:31:37 -0500

    Navigate to sw-run from sw-run-item

--- src/components/sw-run-item.vue
+++ src/components/sw-run-item.vue
@@ -3,7 +3,7 @@
     <div class="sw-card">
       <div class="sw-run-text">{{ run.name }}</div>
       <div class="sw-run-timer">{{ runSecondsClock }}</div>
-      <button class="sw-button">Start</button>
+      <button v-on:click="viewDetails()" class="sw-button">Details</button>
     </div>
   </li>
 </template>
@@ -27,6 +27,16 @@ export default {
       }
       var start = (total >= 3600 ? 11 : 14)
       return new Date(1000 * total).toISOString().slice(start, 19)
+    }
+  },
+  methods: {
+    viewDetails () {
+      this.$router.push({
+        name: 'sw-run',
+        params: {
+          id: this.run.id
+        }
+      })
     }
   }
 }
