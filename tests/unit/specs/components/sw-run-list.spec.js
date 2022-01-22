372ef9add2d17728b86117192c2b6f4a9cb593a8
Robert Corrigan <rjcorrig@gmail.com>
2022-01-21 22:27:03 -0500

    Fix sw-run-list tests

--- tests/unit/specs/components/sw-run-list.spec.js
+++ tests/unit/specs/components/sw-run-list.spec.js
@@ -1,41 +1,36 @@
+import Vuex from 'vuex'
 import { expect } from 'chai'
 import { createLocalVue, mount } from '@vue/test-utils'
 import sinon from 'sinon'
 import swRunList from '@/components/sw-run-list'
 import seedData from '@/stepwatch/models/seedData'
 import VueRouter from 'vue-router'
-import services from '@/plugins/services'
-import DataStore from '@/stepwatch/services/datastore'
+import createStore from '@/store'
 
 const localVue = createLocalVue()
-
-const dataStore = new DataStore()
-localVue.use(services, {
-  dataStore: dataStore
-})
+localVue.use(Vuex)
 localVue.use(VueRouter)
 
 describe('sw-run-list.vue', () => {
   let wrapper
+  let store
 
   beforeEach(() => {
     // Reset the test data
-    wrapper = mount(swRunList, { localVue })
-    wrapper.vm.$services.dataStore.seed(seedData())
-    wrapper.destroy()
+    store = createStore({ runs: seedData() })
   })
 
   afterEach(() => {
-    wrapper.destroy()
+    // wrapper.destroy()
   })
 
   describe('constructor', () => {
     it('should list all runs if no props passed', () => {
-      wrapper = mount(swRunList, { localVue })
+      wrapper = mount(swRunList, { localVue, store })
       expect(wrapper.element.querySelector('.sw-page-title').textContent)
         .to.equal('All runs and programs')
       expect(wrapper.element.querySelectorAll('li.sw-run-item').length)
-        .to.equal(wrapper.vm.$services.dataStore.getRuns().length)
+        .to.equal(wrapper.vm.$store.getters.getRuns().length)
     })
 
     it('should list only filtered runs if filter was passed', () => {
@@ -43,18 +38,18 @@ describe('sw-run-list.vue', () => {
         title: 'pie',
         filter: r => r.name.indexOf('pie') >= 0
       }
-      wrapper = mount(swRunList, { localVue, propsData })
+      wrapper = mount(swRunList, { localVue, propsData, store })
       expect(wrapper.element.querySelector('.sw-page-title').textContent)
         .to.equal(propsData.title)
       expect(wrapper.element.querySelectorAll('li.sw-run-item').length)
-        .to.equal(wrapper.vm.$services.dataStore.getRuns(propsData.filter).length)
+        .to.equal(wrapper.vm.$store.getters.getRuns(propsData.filter).length)
     })
 
     it('shows the Create New button for program list', () => {
       const propsData = {
         type: 'program'
       }
-      wrapper = mount(swRunList, { localVue, propsData })
+      wrapper = mount(swRunList, { localVue, propsData, store })
       expect(wrapper.element.querySelector('.sw-header > .sw-action-button i[title="New"]'))
         .to.not.equal(null)
     })
@@ -63,109 +58,80 @@ describe('sw-run-list.vue', () => {
       const propsData = {
         type: 'pies'
       }
-      wrapper = mount(swRunList, { localVue, propsData })
+      wrapper = mount(swRunList, { localVue, propsData, store })
       expect(wrapper.element.querySelector('.sw-header > .sw-action-button i[title="New"]'))
         .to.equal(null)
     })
   })
 
   describe('newProgram', () => {
-    it('creates a new program and saves it to the data store', () => {
-      wrapper = mount(swRunList, { localVue })
+    it('creates a new program', async () => {
+      wrapper = mount(swRunList, { localVue, store })
 
       const count = wrapper.vm.runs.length
-      try {
-        const stub = sinon.stub(wrapper.vm.$services.dataStore, 'save')
 
-        wrapper.vm.newProgram()
-        expect(wrapper.vm.runs.length).to.equal(count + 1)
-        expect(stub.called).to.equal(true)
-      } finally {
-        wrapper.vm.$services.dataStore.save.restore()
-      }
+      await wrapper.vm.newProgram()
+      expect(wrapper.vm.runs.length).to.equal(count + 1)
     })
   })
 
   describe('copy', () => {
-    it('copies the current run and inserts new after current', () => {
-      wrapper = mount(swRunList, { localVue })
+    it('copies the current run and inserts new after current', async () => {
+      wrapper = mount(swRunList, { localVue, store })
 
       const count = wrapper.vm.runs.length
       const index = 2
       const original = wrapper.vm.runs[index]
 
-      try {
-        const stub = sinon.stub(wrapper.vm.$services.dataStore, 'save')
-        const copy = wrapper.vm.copy(original)
+      const copy = await wrapper.vm.copy(original)
 
-        expect(wrapper.vm.runs.length).to.equal(count + 1)
-        expect(wrapper.vm.runs.indexOf(copy)).to.equal(index + 1)
-        expect(wrapper.vm.runs.indexOf(original)).to.equal(index)
-        expect(stub.called).to.equal(true)
-      } finally {
-        wrapper.vm.$services.dataStore.save.restore()
-      }
+      expect(wrapper.vm.runs.length).to.equal(count + 1)
+      expect(wrapper.vm.runs.indexOf(copy)).to.equal(index + 1)
+      expect(wrapper.vm.runs.indexOf(original)).to.equal(index)
     })
 
-    it('sets status of copy of program to "program"', () => {
+    it('sets status of copy of program to "program"', async () => {
       const propsData = {
         type: 'program',
         filter: r => r.status === 'program'
       }
-      wrapper = mount(swRunList, { localVue, propsData })
+      wrapper = mount(swRunList, { localVue, propsData, store })
 
       const index = 0
       const original = wrapper.vm.runs[index]
 
-      try {
-        sinon.stub(wrapper.vm.$services.dataStore, 'save')
-        const copy = wrapper.vm.copy(original)
+      const copy = await wrapper.vm.copy(original)
 
-        expect(copy.status).to.equal('program')
-      } finally {
-        wrapper.vm.$services.dataStore.save.restore()
-      }
+      expect(copy.status).to.equal('program')
     })
 
-    it('sets status of copy of run to "created"', () => {
+    it('sets status of copy of run to "created"', async () => {
       const propsData = {
         type: 'running',
         filter: r => ['paused', 'running', 'created'].indexOf(r.status) >= 0
       }
-      wrapper = mount(swRunList, { localVue, propsData })
+      wrapper = mount(swRunList, { localVue, propsData, store })
 
       const index = 0
       const original = wrapper.vm.runs[index]
 
-      try {
-        sinon.stub(wrapper.vm.$services.dataStore, 'save')
-        const copy = wrapper.vm.copy(original)
+      const copy = await wrapper.vm.copy(original)
 
-        expect(copy.status).to.equal('created')
-      } finally {
-        wrapper.vm.$services.dataStore.save.restore()
-      }
+      expect(copy.status).to.equal('created')
     })
   })
 
   describe('remove', () => {
-    it('deletes the selected run and saves the data store', () => {
-      wrapper = mount(swRunList, { localVue })
+    it('deletes the selected run and saves the data store', async () => {
+      wrapper = mount(swRunList, { localVue, store })
 
       const count = wrapper.vm.runs.length
       const index = 2
       const original = wrapper.vm.runs[index]
 
-      try {
-        const stub = sinon.stub(wrapper.vm.$services.dataStore, 'save')
-
-        wrapper.vm.remove(original)
-        expect(wrapper.vm.runs.length).to.equal(count - 1)
-        expect(wrapper.vm.runs.indexOf(original)).to.equal(-1)
-        expect(stub.called).to.equal(true)
-      } finally {
-        wrapper.vm.$services.dataStore.save.restore()
-      }
+      await wrapper.vm.remove(original)
+      expect(wrapper.vm.runs.length).to.equal(count - 1)
+      expect(wrapper.vm.runs.indexOf(original)).to.equal(-1)
     })
   })
 
@@ -178,7 +144,7 @@ describe('sw-run-list.vue', () => {
     })
 
     it('routes to the sw-run/{{ run.id }}', () => {
-      wrapper = mount(swRunList, { localVue, router })
+      wrapper = mount(swRunList, { localVue, router, store })
 
       const run = wrapper.vm.runs[2]
       wrapper.vm.viewDetails(run)
@@ -199,25 +165,18 @@ describe('sw-run-list.vue', () => {
       spy = sinon.stub(router, 'push')
     })
 
-    it('copies the current run, starts the copy, and routes to it', () => {
-      wrapper = mount(swRunList, { localVue, router })
+    it('copies the current run, starts the copy, and routes to it', async () => {
+      wrapper = mount(swRunList, { localVue, router, store })
 
       const index = 2
       const original = wrapper.vm.runs[index]
 
-      try {
-        const stub = sinon.stub(wrapper.vm.$services.dataStore, 'save')
-        const copy = wrapper.vm.create(original)
+      const copy = await wrapper.vm.create(original)
 
-        expect(copy.status).to.equal('running')
+      expect(copy.status).to.equal('running')
 
-        expect(stub.called).to.equal(true)
-
-        expect(spy.firstCall.args[0].name).to.equal('sw-run')
-        expect(spy.firstCall.args[0].params.id).to.equal(copy.id)
-      } finally {
-        wrapper.vm.$services.dataStore.save.restore()
-      }
+      expect(spy.firstCall.args[0].name).to.equal('sw-run')
+      expect(spy.firstCall.args[0].params.id).to.equal(copy.id)
     })
 
     afterEach(() => {
